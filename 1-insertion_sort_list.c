54b61e5f1c33f6d3b4ccf218b528ae6a5216975c
DaisyAram <daisyarama03@gmail.com>
2024-01-22 02:07:47 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,41 @@
 #include "sort.h"
-
-/**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       using the insertion sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
+ *
+ * Return: nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *itr, *next, *current;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+	if (list == NULL || *list == NULL)/*list is null*/
+	return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	current = ((*list)->next);
+	while (current != NULL)/*current node is not null*/
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		next = current->next;
+		itr = current->prev;
+
+		while (itr != NULL && itr->n > current->n)/*iterator is not null*/
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			itr->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = itr;
+			current->prev = itr->prev;
+			current->next = itr;
+			itr->prev = current;
+			if (current->prev != NULL)
+				current->prev->next = current;
+			else
+				*list = current;
+			itr = current->prev;
+			print_list(*list);
 		}
+		current = next;
 	}
 }
